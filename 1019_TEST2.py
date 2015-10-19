48a2e91bdea484240247c6ea3c205e91ee373806
DonutZhang <b10231034@mail.ntust.edu.tw>
2015-10-19 15:14:34 +0800

    test

---
+++ 1019_TEST2.py
@@ -0,0 +1,194 @@
+
+# coding: utf-8
+
+# In[1]:
+
+'snap'
+
+
+# In[2]:
+
+"crackle"
+
+
+# In[3]:
+
+"中"
+
+
+# In[4]:
+
+'中'
+
+
+# In[5]:
+
+"'nepd,'dfsf"
+
+
+# In[6]:
+
+"""sdsf"""
+
+
+# In[7]:
+
+'wefe sfd : ".'
+
+
+# In[9]:
+
+'A "two by four" is " 2 x 4 ".'
+
+
+# In[10]:
+
+"this's 'the men' ad"
+
+
+# In[11]:
+
+'''sd'''
+
+
+# In[12]:
+
+"""sdsd"""
+
+
+# In[17]:
+
+joke = """有一天，
+小明的媽媽叫他每天都要去山上找三句好話，
+可是媽媽有一天媽媽生病了"""
+
+print(joke)
+
+
+# In[18]:
+
+joke = """有一天，
+小明的媽媽叫他每天都要去山上找三句好話，
+可是媽媽有一天媽媽生病了"""
+
+joke
+
+
+# In[20]:
+
+bottles = 99
+base = ''
+base += '現有存量:'
+base += str(bottles)
+base
+
+
+# In[23]:
+
+pic = 'A man ,\n A Plan'
+print(pic)
+
+
+# In[24]:
+
+print('\tabc')
+
+
+# In[26]:
+
+print('a\tcb')
+
+
+# In[27]:
+
+print('ab\tc')
+
+
+# In[28]:
+
+print('\"asa\"')
+
+
+# In[30]:
+
+"no" + " problem"
+
+
+# In[31]:
+
+print("no" + " problem")
+
+
+# In[35]:
+
+start = 'Na ' * 4 + '\n'
+mid = 'Hey ' * 3 + '\n'
+print(start + mid)
+
+
+# In[37]:
+
+let = '1234567890122334455'
+let[:]
+
+
+# In[38]:
+
+let[4:]
+
+
+# In[39]:
+
+let[:10]
+
+
+# In[40]:
+
+let[12:20]
+
+
+# In[41]:
+
+let[-5]
+
+
+# In[42]:
+
+let[-5:]
+
+
+# In[44]:
+
+let[10:-3]
+
+
+# In[45]:
+
+let[::7]
+
+
+# In[46]:
+
+let[4:20:3]
+
+
+# In[49]:
+
+let[10::4]
+
+
+# In[50]:
+
+let[:20:3]
+
+
+# In[54]:
+
+name = '小旻在睡覺'
+name.replace ('小','大')
+
+
+# In[ ]:
+
+
+
